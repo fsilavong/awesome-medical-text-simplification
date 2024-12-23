e5b73d9027c62789733be40bbef62a2c328e24ff
Fran <fransilavong@gmail.com>
2024-12-23 05:25:48 +0000

    first commit

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+    </h1>
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
