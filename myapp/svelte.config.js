5b47e264c2fe5a2d959209eae749265bf73cfbea
Fran <fransilavong@gmail.com>
2024-12-25 03:41:58 +0000

    static building

--- myapp/svelte.config.js
+++ myapp/svelte.config.js
@@ -1,12 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
@@ -21,7 +16,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+			base: process.env.NODE_ENV === "production" ? "/awesome-medical-text-simplification" : "",
 		},
 	}
 };
